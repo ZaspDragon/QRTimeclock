c3bc8a7b0fefc7875e63dd0ea16e481256032856
ZaspDragon <ilevanshine@gmail.com>
2026-04-13 15:21:00 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyB4xdaxbkXDRILPe2nGZuGCS-PXf35bk3o",
+  authDomain: "qrtimeclock-42764.firebaseapp.com",
+  projectId: "qrtimeclock-42764",
+  storageBucket: "qrtimeclock-42764.appspot.com",
+  messagingSenderId: "232535382723",
+  appId: "1:232535382723:web:9fe08f4961d87ba4062076"
 };
 
 export const appSettings = {
-  companyName: "Your Company",
+  companyName: "Chadwell",
   defaultAppUrl: "https://YOUR_PROJECT.web.app"
 };
