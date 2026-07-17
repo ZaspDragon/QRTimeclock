097f26f3b95d442dd1fad4a599fe49a8da48b2a7
Brandon E <ilevanshine@gmail.com>
2026-07-17 17:45:27 -0400

    Load new worker first-punch hotfix

--- firebase-config.js
+++ firebase-config.js
@@ -26,5 +26,8 @@ if (typeof window !== 'undefined') {
     import('./lunch-labels.js?v=20260711-1').catch((error) => {
       console.warn('Lunch label update failed to load:', error.message);
     });
+    import('./new-worker-first-punch-hotfix.js?v=20260717-1').catch((error) => {
+      console.warn('New-worker first-punch hotfix failed to load:', error.message);
+    });
   });
 }
