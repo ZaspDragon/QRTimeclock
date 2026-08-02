e2855239a2f79e5bdbabc73117cb5d749d97aff8
Brandon E <ilevanshine@gmail.com>
2026-08-01 23:50:40 -0400

    Disable undeployed public time lookup hotfix

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 import './theme-toggle.js?v=20260728-2';
 import './public-clock-context.js?v=20260801-2';
-import './name-only-time-lookup.js?v=20260801-1';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyB4xdaxbkXDRILPe2nGZuGCS-PXf35bk3o",
